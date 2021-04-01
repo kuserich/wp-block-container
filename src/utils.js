1c9d7420756ac19ca0f090cc8933f5bc59f83d02
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-04-02 00:52:07 +0300

    Added helper variables

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+/**
+ * Helpers.
+ */
+const utils = {
+	thresholds: {
+		width: {
+			min: 100,
+			max: 2000,
+		},
+		dim: {
+			min: 0,
+			max: 100,
+			step: 10,
+		},
+	},
+};
+
+export default utils;
