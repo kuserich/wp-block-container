87117df74ad4fc23ea5015d77f1f7d50902392ea
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-04-13 17:08:21 +0200

    Webpack config cleanup and linting

--- submodule-webpack.config.js
+++ submodule-webpack.config.js
@@ -2,35 +2,23 @@
  * Standard JavaScript compile functionality for a block module (git submodule).
  * This webpack config is used in conjunction with custom 'wp-scripts build' || 'wp-scripts start' scripts by passing a --config argument.
  * The plugin below works in 'build' as well as 'start' with the watcher.
- * 
+ *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-scripts/#provide-your-own-webpack-config
  */
 
-const defaultConfig = require('./node_modules/@wordpress/scripts/config/webpack.config.js');
-const exec = require('child_process').exec;
-const { stdout, stderr } = require('process');
+const defaultConfig = require( './node_modules/@wordpress/scripts/config/webpack.config.js' );
+const childProc = require( 'child_process' );
 
 module.exports = {
 	...defaultConfig,
-	module: {
-		...defaultConfig.module,
-		rules: [...defaultConfig.module.rules],
-	},
-	optimization: {
-		...defaultConfig.optimization,
-		minimizer: [...defaultConfig.optimization.minimizer],
-	},
-	performance: {
-		hints: false,
-	},
 	plugins: [
 		...defaultConfig.plugins,
 		{
-			apply: (compiler) => {
-				compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-					exec('../../prepare-blocks.sh', (err, stdout, stderr) => {
-						if (stdout) process.stdout.write(stdout);
-						if (stderr) process.stderr.write(stderr);
+			apply: ( compiler ) => {
+				compiler.hooks.afterEmit.tap( 'AfterEmitPlugin', () => {
+					childProc.exec( '../../prepare-blocks.sh', ( stdout, stderr ) => {
+						if ( stdout ) process.stdout.write( stdout );
+						if ( stderr ) process.stderr.write( stderr );
 					});
 				});
 			},
