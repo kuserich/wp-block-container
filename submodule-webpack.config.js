cb0c017b05518323d2a49bf34f56d1c2f2e316f1
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-04-16 15:33:15 +0200

    Add condition to use config file if it exists otherwise use wordpress default

--- submodule-webpack.config.js
+++ submodule-webpack.config.js
@@ -1,12 +1,16 @@
 /**
  * Standard JavaScript compile functionality for a block module (git submodule).
  * This webpack config is used in conjunction with custom 'wp-scripts build' || 'wp-scripts start' scripts by passing a --config argument.
+ * If there is already a webpack.config.js file in the root folder - we use that as the default config, otherwise we use the one provided by wordpress.
  * The plugin below works in 'build' as well as 'start' with the watcher.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-scripts/#provide-your-own-webpack-config
  */
 
-const defaultConfig = require( './node_modules/@wordpress/scripts/config/webpack.config.js' );
+const fs = require( 'fs' );
+const defaultConfig = require( fs.existsSync( './webpack.config.js' )
+	? './webpack.config.js'
+	: './node_modules/@wordpress/scripts/config/webpack.config.js' );
 const childProc = require( 'child_process' );
 
 module.exports = {
