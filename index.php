6dfaddee9ce872fbff381c976bf6e2cc9c95f753
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-01 12:31:45 +0300

    Use namespace in registering block type

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
 /**
- * Plugin Name:     Sixa - Container
- * Description:     Container block for WordPress editor.
- * Version:         0.1.0
- * Author:          sixa AG
- * License:         GPL-3.0-or-later
- * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:     sixa
+ * Plugin Name:         Sixa - Container
+ * Description:         Container block for WordPress editor.
+ * Version:             0.2.0
+ * Author:              sixa AG
+ * License:             GPL-3.0-or-later
+ * License URI:         https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:         sixa
+ * Requires at least:   WordPress 5.6
+ * Requires PHP:        7.2
  *
- * @package         sixa
+ * @package         	sixa
  */
+
+namespace SixaContainerBlock;
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
@@ -18,7 +22,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
-function sixa_wp_block_container_init() {
+function register_block() {
 	register_block_type_from_metadata( __DIR__ );
 }
-add_action( 'init', 'sixa_wp_block_container_init' );
+add_action( 'init', __NAMESPACE__ . '\register_block' );
