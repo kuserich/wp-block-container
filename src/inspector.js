b4bb512e1ef8ce3299a3dc9cea033f8ba4d432fc
Sajedeh Gooklani <gookalani@mail.com>
2021-08-19 19:42:05 +0300

    Added `Container title` option to the advanced controls

--- src/inspector.js
+++ src/inspector.js
@@ -23,7 +23,12 @@ import { __ } from '@wordpress/i18n';
  *
  * @see    https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
  */
-import { InspectorControls, ContrastChecker, __experimentalPanelColorGradientSettings as PanelColorGradientSettings } from '@wordpress/block-editor';
+import {
+	InspectorControls,
+	InspectorAdvancedControls,
+	ContrastChecker,
+	__experimentalPanelColorGradientSettings as PanelColorGradientSettings,
+} from '@wordpress/block-editor';
 
 /**
  * This packages includes a library of generic WordPress components to be used for
@@ -31,7 +36,7 @@ import { InspectorControls, ContrastChecker, __experimentalPanelColorGradientSet
  *
  * @see    https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
  */
-import { PanelBody, PanelRow, Button, ToggleControl, RangeControl, FocalPointPicker } from '@wordpress/components';
+import { PanelBody, PanelRow, Button, ToggleControl, TextControl, RangeControl, FocalPointPicker } from '@wordpress/components';
 
 /**
  * Inspector Controls appear in the post settings sidebar when a block is being edited.
@@ -63,129 +68,139 @@ export default function Inspector( {
 	utils,
 } ) {
 	const thresholds = get( utils, 'thresholds' );
-	const { url, width, hasParallax, isRepeated, dimRatio, focalPoint, backgroundSize, minHeight } = attributes;
+	const { url, width, hasParallax, isRepeated, dimRatio, focalPoint, backgroundSize, minHeight, title } = attributes;
 	const { setGradient, gradientValue } = useGradient;
 	const showFocalPointPicker = isVideoBackground || ( isImageBackground && ( ! hasParallax || isRepeated ) );
 
 	return (
-		<InspectorControls>
-			<PanelBody initialOpen>
-				<RangeControl
-					allowReset
-					value={ width }
-					label={ __( 'Width', 'sixa' ) }
-					help={ __( 'in pixels', 'sixa' ) }
-					min={ get( thresholds, 'width.min' ) }
-					max={ get( thresholds, 'width.max' ) }
-					onChange={ ( value ) => setAttributes( { width: value } ) }
-				/>
-				<RangeControl
-					allowReset
-					value={ minHeight }
-					label={ __( 'Min. Height', 'sixa' ) }
-					help={ __( 'in pixels', 'sixa' ) }
-					min={ get( thresholds, 'height.min' ) }
-					max={ get( thresholds, 'height.max' ) }
-					onChange={ ( value ) => setAttributes( { minHeight: value } ) }
-				/>
-			</PanelBody>
-			{ !! url && (
-				<PanelBody title={ __( 'Media Settings', 'sixa' ) } initialOpen>
-					{ isImageBackground && (
-						<>
-							<ToggleControl
-								label={ __( 'Fixed background', 'sixa' ) }
-								checked={ hasParallax }
-								onChange={ () =>
-									setAttributes( {
-										hasParallax: ! hasParallax,
-										...( ! hasParallax ? { focalPoint: noop() } : {} ),
-									} )
-								}
-							/>
-							<ToggleControl
-								label={ __( 'Repeated background', 'sixa' ) }
-								checked={ isRepeated }
-								onChange={ () => setAttributes( { isRepeated: ! isRepeated } ) }
-							/>
-							<BackgroundSizeControl
-								label={ __( 'Background Size', 'sixa' ) }
-								value={ backgroundSize }
-								onChange={ ( value ) => setAttributes( { backgroundSize: value } ) }
-							/>
-						</>
-					) }
-					{ showFocalPointPicker && (
-						<FocalPointPicker
-							label={ __( 'Focal point picker', 'sixa' ) }
-							url={ url }
-							value={ focalPoint }
-							onChange={ ( value ) => setAttributes( { focalPoint: value } ) }
-						/>
-					) }
-					<PanelRow>
-						<Button
-							isSmall
-							isDestructive
-							className="components-button--reset-media"
-							onClick={ () =>
-								setAttributes( {
-									url: noop(),
-									id: noop(),
-									backgroundType: noop(),
-									backgroundSize: noop(),
-									dimRatio: noop(),
-									focalPoint: noop(),
-									hasParallax: noop(),
-									isRepeated: noop(),
-								} )
-							}
-						>
-							{ __( 'Clear Media', 'sixa' ) }
-						</Button>
-					</PanelRow>
-				</PanelBody>
-			) }
-			<PanelColorGradientSettings
-				title={ __( 'Color Settings', 'sixa' ) }
-				initialOpen={ false }
-				settings={ [
-					{
-						label: __( 'Text', 'sixa' ),
-						colorValue: get( textColor, 'color' ),
-						onColorChange: setTextColor,
-					},
-					{
-						label: __( 'Overlay', 'sixa' ),
-						colorValue: get( overlayColor, 'color' ),
-						gradientValue,
-						onColorChange: setOverlayColor,
-						onGradientChange: setGradient,
-					},
-				] }
-			>
-				{ !! url && (
+		<>
+			<InspectorControls>
+				<PanelBody initialOpen>
 					<RangeControl
 						allowReset
-						required
-						readonly
-						label={ __( 'Opacity', 'sixa' ) }
-						value={ dimRatio }
-						resetFallbackValue={ get( thresholds, 'dim.min' ) }
-						min={ get( thresholds, 'dim.min' ) }
-						max={ get( thresholds, 'dim.max' ) }
-						step={ get( thresholds, 'dim.step' ) }
-						onChange={ ( value ) => setAttributes( { dimRatio: value } ) }
+						value={ width }
+						label={ __( 'Width', 'sixa' ) }
+						help={ __( 'in pixels', 'sixa' ) }
+						min={ get( thresholds, 'width.min' ) }
+						max={ get( thresholds, 'width.max' ) }
+						onChange={ ( value ) => setAttributes( { width: value } ) }
 					/>
+					<RangeControl
+						allowReset
+						value={ minHeight }
+						label={ __( 'Min. Height', 'sixa' ) }
+						help={ __( 'in pixels', 'sixa' ) }
+						min={ get( thresholds, 'height.min' ) }
+						max={ get( thresholds, 'height.max' ) }
+						onChange={ ( value ) => setAttributes( { minHeight: value } ) }
+					/>
+				</PanelBody>
+				{ !! url && (
+					<PanelBody title={ __( 'Media Settings', 'sixa' ) } initialOpen>
+						{ isImageBackground && (
+							<>
+								<ToggleControl
+									label={ __( 'Fixed background', 'sixa' ) }
+									checked={ hasParallax }
+									onChange={ () =>
+										setAttributes( {
+											hasParallax: ! hasParallax,
+											...( ! hasParallax ? { focalPoint: noop() } : {} ),
+										} )
+									}
+								/>
+								<ToggleControl
+									label={ __( 'Repeated background', 'sixa' ) }
+									checked={ isRepeated }
+									onChange={ () => setAttributes( { isRepeated: ! isRepeated } ) }
+								/>
+								<BackgroundSizeControl
+									label={ __( 'Background Size', 'sixa' ) }
+									value={ backgroundSize }
+									onChange={ ( value ) => setAttributes( { backgroundSize: value } ) }
+								/>
+							</>
+						) }
+						{ showFocalPointPicker && (
+							<FocalPointPicker
+								label={ __( 'Focal point picker', 'sixa' ) }
+								url={ url }
+								value={ focalPoint }
+								onChange={ ( value ) => setAttributes( { focalPoint: value } ) }
+							/>
+						) }
+						<PanelRow>
+							<Button
+								isSmall
+								isDestructive
+								className="components-button--reset-media"
+								onClick={ () =>
+									setAttributes( {
+										url: noop(),
+										id: noop(),
+										backgroundType: noop(),
+										backgroundSize: noop(),
+										dimRatio: noop(),
+										focalPoint: noop(),
+										hasParallax: noop(),
+										isRepeated: noop(),
+									} )
+								}
+							>
+								{ __( 'Clear Media', 'sixa' ) }
+							</Button>
+						</PanelRow>
+					</PanelBody>
 				) }
-				<ContrastChecker
-					{ ...{
-						textColor: get( textColor, 'color' ),
-						backgroundColor: get( overlayColor, 'color' ),
-					} }
-					isLargeText={ false }
+				<PanelColorGradientSettings
+					title={ __( 'Color Settings', 'sixa' ) }
+					initialOpen={ false }
+					settings={ [
+						{
+							label: __( 'Text', 'sixa' ),
+							colorValue: get( textColor, 'color' ),
+							onColorChange: setTextColor,
+						},
+						{
+							label: __( 'Overlay', 'sixa' ),
+							colorValue: get( overlayColor, 'color' ),
+							gradientValue,
+							onColorChange: setOverlayColor,
+							onGradientChange: setGradient,
+						},
+					] }
+				>
+					{ !! url && (
+						<RangeControl
+							allowReset
+							required
+							readonly
+							label={ __( 'Opacity', 'sixa' ) }
+							value={ dimRatio }
+							resetFallbackValue={ get( thresholds, 'dim.min' ) }
+							min={ get( thresholds, 'dim.min' ) }
+							max={ get( thresholds, 'dim.max' ) }
+							step={ get( thresholds, 'dim.step' ) }
+							onChange={ ( value ) => setAttributes( { dimRatio: value } ) }
+						/>
+					) }
+					<ContrastChecker
+						{ ...{
+							textColor: get( textColor, 'color' ),
+							backgroundColor: get( overlayColor, 'color' ),
+						} }
+						isLargeText={ false }
+					/>
+				</PanelColorGradientSettings>
+			</InspectorControls>
+			<InspectorAdvancedControls>
+				<TextControl
+					label={ __( 'Container title', 'sixa' ) }
+					help={ __( 'Enter a title for this section to display on the left side of the container.', 'sixa' ) }
+					value={ title }
+					onChange={ ( value ) => setAttributes( { title: value } ) }
 				/>
-			</PanelColorGradientSettings>
-		</InspectorControls>
+			</InspectorAdvancedControls>
+		</>
 	);
 }
