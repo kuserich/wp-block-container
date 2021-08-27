1ec5f34522b9f4d811ea24ed14b86af8725cf69d
Markus <markus.goeckeritz@sixa.ch>
2021-08-27 09:56:34 +0200

    added block class

---
+++ block/ContainerBlock.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Sixa;
+
+class ContainerBlock {
+
+	public function init(): void {
+		add_action( 'init', array( $this, 'register' ) );
+	}
+
+	public function register(): void {
+		register_block_type_from_metadata(
+			dirname( __DIR__ ),
+			array(
+				'render_callback' => array( $this, 'render' ),
+			)
+		);
+	}
+
+	/**
+	 * Modifies the `sixa/container` block output.
+	 *
+	 * @since     1.0.0
+	 * @param     array  $attributes        The block attributes.
+	 * @param     string $content           The block content.
+	 * @return    string
+	 */
+	public function render( array $attributes = array(), string $content ): string {
+		libxml_use_internal_errors( true );
+		$dom = new \DOMDocument();
+		$dom->loadHTML( $content, LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED );
+		$xpath = new \DomXPath( $dom );
+		$node  = $xpath->query( "//div[contains(@class, 'wp-block-sixa-container')]" );
+
+		if ( $node ) {
+			$before_content = apply_filters( 'sixa_container_block_before_content', __return_empty_string(), $attributes );
+			$after_content  = apply_filters( 'sixa_container_block_after_content', __return_empty_string(), $attributes );
+
+			if ( ! empty( $before_content ) ) {
+				$before_content_fragment = $dom->createDocumentFragment();
+				$before_content_fragment->appendXML( $before_content );
+				$node->item( 0 )->insertBefore( $before_content_fragment, $node->item( 0 )->firstChild );
+			}
+
+			if ( ! empty( $after_content ) ) {
+				$after_content_fragment = $dom->createDocumentFragment();
+				$after_content_fragment->appendXML( $after_content );
+				$node->item( 0 )->insertBefore( $after_content_fragment );
+			}
+
+			libxml_clear_errors();
+			$content = $dom->saveHTML();
+		}
+
+		return $content;
+	}
+
+}
