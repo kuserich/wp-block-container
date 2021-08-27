01536a8dea0fbfa91cd7b23b46a28db929fd3015
Markus <markus.goeckeritz@sixa.ch>
2021-08-27 09:56:43 +0200

    updated index.php for block class

--- index.php
+++ index.php
@@ -16,63 +16,7 @@
  * @package             sixa
  */
 
-namespace SixaContainerBlock;
+use Sixa\ContainerBlock;
 
-defined( 'ABSPATH' ) || exit;
-
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see       https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
- * @since     1.0.0
- * @return    void
- */
-function register_block(): void {
-	register_block_type_from_metadata(
-		__DIR__,
-		array(
-			'render_callback' => __NAMESPACE__ . '\render_block',
-		)
-	);
-}
-add_action( 'init', __NAMESPACE__ . '\register_block' );
-
-/**
- * Modifies the `sixa/container` block output.
- *
- * @since     1.0.0
- * @param     array  $attributes        The block attributes.
- * @param     string $content           The block content.
- * @return    string
- */
-function render_block( array $attributes = array(), string $content ): string {
-	libxml_use_internal_errors( true );
-	$dom = new \DOMDocument();
-	$dom->loadHTML( $content, LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED );
-	$xpath = new \DomXPath( $dom );
-	$node  = $xpath->query( "//div[contains(@class, 'wp-block-sixa-container')]" );
-
-	if ( $node ) {
-		$before_content = apply_filters( 'sixa_container_block_before_content', __return_empty_string(), $attributes );
-		$after_content  = apply_filters( 'sixa_container_block_after_content', __return_empty_string(), $attributes );
-
-		if ( ! empty( $before_content ) ) {
-			$before_content_fragment = $dom->createDocumentFragment();
-			$before_content_fragment->appendXML( $before_content );
-			$node->item( 0 )->insertBefore( $before_content_fragment, $node->item( 0 )->firstChild );
-		}
-
-		if ( ! empty( $after_content ) ) {
-			$after_content_fragment = $dom->createDocumentFragment();
-			$after_content_fragment->appendXML( $after_content );
-			$node->item( 0 )->insertBefore( $after_content_fragment );
-		}
-
-		libxml_clear_errors();
-		$content = $dom->saveHTML();
-	}
-
-	return $content;
-}
+$block = new ContainerBlock();
+$block->init();
