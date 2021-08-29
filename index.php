649661298c32b136f9f96d9701924d0152086746
Markus <markus.goeckeritz@sixa.ch>
2021-08-29 23:57:52 +0200

    added comments

--- index.php
+++ index.php
@@ -16,11 +16,27 @@
  * @package             sixa
  */
 
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+/**
+ * Include the namespace of this block.
+ */
 use Sixa_Blocks\Container;
 
-// Composer autoload is needed in this package even if
-// it doesn't use any libraries to autoload the classes
-// from this package.
+/**
+ * Composer autoload is needed in this package even if
+ * it doesn't use any libraries to autoload the classes
+ * from this package.
+ *
+ * @see    https://getcomposer.org/doc/01-basic-usage.md#autoloading
+ */
 require __DIR__ . '/vendor/autoload.php';
 
+/**
+ * Initialize your block.
+ *
+ * Other than this function call, this file should not include any logic
+ * and should merely be used as an entry point to use this package as
+ * a WordPress plugin.
+ */
 Container::init();
