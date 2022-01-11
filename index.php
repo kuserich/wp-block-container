de259d5bb174e136aad36a1e7cd2e3c61b2f44fa
r007 <moninsergei@gmail.com>
2022-01-11 14:59:25 +0400

    Merge readme.txt into README.md, fix meta properties in index.php

--- index.php
+++ index.php
@@ -4,15 +4,20 @@
  *
  * @wordpress-plugin
  * Plugin Name:          Sixa - Container Block
- * Description:          Wrap several blocks in a parent wrapper and do more styling.
+ * Description:          A fully customizable wrapper block to group other blocks together. Create more sophisticated designs with more space, colors, gradients, background images, videos, and much more.
+ * Tags:                 container, group, section, wrapper, block, gutenberg, sixa
+ * Contributors:         sixa, mahdiyazdani, gookaani, kuserich
  * Version:              1.1.0
+ * Stable tag:           1.1.0
  * Requires at least:    5.7
  * Requires PHP:         7.3
+ * Tested up to:         5.8
  * Author:               sixa AG
  * Author URI:           https://sixa.com
- * License:              GPL v3 or later
+ * License:              GPLv3 or later
  * License URI:          https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:          sixa-block-container
+ * Donate link:          https://sixa.com/
  *
  * @package              Sixa_Blocks
  */
