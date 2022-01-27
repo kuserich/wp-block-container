f366ccd55b1d633e6024cfeb1842868cc9d0f7d5
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-01-27 20:53:56 +0300

    Imported & integrated `MediaUploadToolbar` from the `components` package

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,14 +1,19 @@
-/* eslint-disable @wordpress/no-unsafe-wp-apis */
+/**
+ * Helper React components specific for Sixa projects.
+ */
+import { BlockAlignmentMatrixControl, FullHeightAlignmentControl, MediaUploadToolbar } from '@sixa/wp-block-components';
 
 /**
- * Utility for libraries from the `Lodash`.
+ * Helper React hooks specific for Sixa projects.
  */
-import isEqual from 'lodash/isEqual';
+import { useDidUpdate, useGetMediaType } from '@sixa/wp-react-hooks';
 
 /**
- * Runtime type checking for React props and similar objects.
+ * This module allows you to create and use standalone block-editor element and components.
+ *
+ * @see    https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/
  */
-import PropTypes from 'prop-types';
+import { BlockControls } from '@wordpress/block-editor';
 
 /**
  * Internationalization utilities for client-side localization.
@@ -18,23 +23,9 @@ import PropTypes from 'prop-types';
 import { __ } from '@wordpress/i18n';
 
 /**
- * Blob utilities for WordPress.
- *
- * @see    https://github.com/WordPress/gutenberg/blob/trunk/packages/blob/README.md
+ * Runtime type checking for React props and similar objects.
  */
-import { isBlobURL, getBlobTypeByURL } from '@wordpress/blob';
-
-/**
- * This module allows you to create and use standalone block-editor element and components.
- *
- * @see    https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/
- */
-import { BlockControls, MediaReplaceFlow } from '@wordpress/block-editor';
-
-/**
- * Helper React components specific for Sixa projects.
- */
-import { BlockAlignmentMatrixControl, FullHeightAlignmentControl } from '@sixa/wp-block-components';
+import PropTypes from 'prop-types';
 
 /**
  * Helper constants.
@@ -52,42 +43,11 @@ import Constants from '../constants';
  */
 function Controls( { attributes, setAttributes } ) {
 	const { contentPosition, id, isFullHeight, url } = attributes;
-	const handleOnSelectMedia = ( media ) => {
-		if ( ! media || ! media?.url ) {
-			setAttributes( { id: undefined, url: undefined } );
-			return;
-		}
+	const backgroundType = useGetMediaType( id );
 
-		if ( isBlobURL( media?.url ) ) {
-			media.type = getBlobTypeByURL( media.url );
-		}
-
-		let mediaType;
-		// for media selections originated from a file upload.
-		if ( media?.media_type ) {
-			if ( isEqual( media.media_type, Constants.IMAGE_MEDIA_TYPE ) ) {
-				mediaType = Constants.IMAGE_MEDIA_TYPE;
-			} else {
-				// only images and videos are accepted so if the media_type is not an image we can assume it is a video.
-				// Videos contain the media type of 'file' in the object returned from the rest api.
-				mediaType = Constants.VIDEO_MEDIA_TYPE;
-			}
-		} else {
-			// for media selections originated from existing files in the media library.
-			if ( ! isEqual( media?.type, Constants.IMAGE_MEDIA_TYPE ) && ! isEqual( media?.type, Constants.VIDEO_MEDIA_TYPE ) ) {
-				return;
-			}
-			mediaType = media?.type;
-		}
-
-		setAttributes( {
-			backgroundType: mediaType,
-			dimRatio: 50,
-			id: media?.id,
-			url: media?.url,
-			...( isEqual( mediaType, Constants.VIDEO_MEDIA_TYPE ) ? { focalPoint: undefined, hasParallax: undefined } : {} ),
-		} );
-	};
+	useDidUpdate( () => {
+		setAttributes( { backgroundType } );
+	}, [ backgroundType ] );
 
 	return (
 		<>
@@ -102,15 +62,12 @@ function Controls( { attributes, setAttributes } ) {
 					onToggle={ () => setAttributes( { isFullHeight: ! isFullHeight, minHeight: undefined } ) }
 				/>
 			</BlockControls>
-			<BlockControls group="other">
-				<MediaReplaceFlow
-					allowedTypes={ [ Constants.IMAGE_MEDIA_TYPE, Constants.VIDEO_MEDIA_TYPE ] }
-					mediaId={ id }
-					mediaURL={ url }
-					name={ ! url ? __( 'Add Media', 'sixa-block-container' ) : __( 'Replace', 'sixa-block-container' ) }
-					onSelect={ handleOnSelectMedia }
-				/>
-			</BlockControls>
+			<MediaUploadToolbar
+				allowedTypes={ [ Constants.IMAGE_MEDIA_TYPE, Constants.VIDEO_MEDIA_TYPE ] }
+				onChange={ ( value ) => setAttributes( { ...value } ) }
+				shouldRender
+				value={ { id, url } }
+			/>
 		</>
 	);
 }
